d13d1d9c4f702f79f5d1ebf59f2d8e0e6254c912
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-06-28 09:27:33 -0400

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Gabriel Marquez on 2022-06-28.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
